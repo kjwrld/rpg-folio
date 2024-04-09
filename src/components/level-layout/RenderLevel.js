35934e901c2a11fd075bfbf23e7633ad4317f594
kaidge <kj@dfrnt.world>
2024-04-08 17:19:47 -0700

    updated --pixel-size to my prefrences

--- src/components/level-layout/RenderLevel.js
+++ src/components/level-layout/RenderLevel.js
@@ -1,18 +1,22 @@
 import LevelBackgroundLayer from "./LevelBackgroundLayer";
 import LevelPlacementsLayer from "./LevelPlacementsLayer";
+import { THEME_BACKGROUNDS } from "../../helpers/constants";
 import styles from "./RenderLevel.module.css";
 
 export default function RenderLayer({ spriteSheet, background }) {
   const level = {
-    // theme: "LEVEL_THEME.MYSTERY_DUNGEON",
+    theme: "office",
     placements: [
       // Level 0
-      { id: "mona", x: -3.5, y: 2, frameCoord: "2x0", size: 32 },
+      { id: "mona", x: -1.5, y: 2.5, frameCoord: "2x0", size: 32 },
       { id: "DL-002", x: 4, y: 1, frameCoord: "0x10", size: 32 },
     ],
   };
   return (
-    <div className={styles.fullScreenContainer}>
+    <div
+      className={styles.fullScreenContainer}
+      style={{ background: THEME_BACKGROUNDS[level.theme] }}
+    >
       <div className={styles.gameScreen}>
         <LevelBackgroundLayer level={level} background={background} />
         <LevelPlacementsLayer level={level} spriteSheet={spriteSheet} />
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,3 +1,24 @@
 export const CELL_SIZE = 16;
 export const SPRITE_SHEET_SRC = "./kj-0001-v2.png";
 export const BACKGROUND_SHEET_SRC = "../background.png";
+
+export const LEVEL_THEMES = {
+  bedroom: "bedroom",
+  office: "office",
+};
+
+export const THEME_BACKGROUNDS = {
+  [LEVEL_THEMES.bedroom]: "#2f2808",
+  [LEVEL_THEMES.office]: "#f2f2f2",
+};
+
+export const THEME_MAP = {
+  [LEVEL_THEMES.office]: {
+    FLOOR: "1x1",
+    TOP: "1x0",
+    LEFT: "0x1",
+    RIGHT: "2x1",
+    BOTTOM: "1x2",
+    WALL: "0x2",
+  },
+};
